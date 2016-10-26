0693af2ebbaa8070579fef43d10feefd1f2c8bdd
nizbridge <nizbridge@gmail.com>
2016-10-26 16:58:04 +0900

    reactjs 스터디

---
+++ reactjs/src/components/Content.js
@@ -0,0 +1,29 @@
+import React from 'react';
+ 
+ 
+class Content extends React.Component {
+    render(){
+        return (
+            <div>
+                <h2>{ this.props.title }</h2>
+                <p> { this.props.body } </p>
+            </div>
+        );
+    }
+}
+
+class ContactInfo extends React.Component {
+    render() {
+        return(
+            <li>{this.props.name} {this.props.phone}</li>
+        );
+    }
+}
+
+Content.propTypes = {
+    title: React.PropTypes.string,
+    body: React.PropTypes.string.isRequired
+};
+
+
+export default Content;
