086db8fea6600a92c98a30be006a9f9a0c8e5c33
kimss <niizguy@duzon.com>
2016-11-24 11:33:56 +0900

    reset

--- reactjs/src/components/App.js
+++ reactjs/src/components/App.js
@@ -4,45 +4,8 @@ class App extends React.Component {
     render(){
  
         return (
-                <Contacts/>
+                <h1>Hello React Skeleton</h1>
         );
-    }
-}
- 
-class Contacts extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            contactData: [
-                {name: "Abet", phone: "010-0000-0001"},
-                {name: "Betty", phone: "010-0000-0002"},
-                {name: "Charlie", phone: "010-0000-0003"},
-                {name: "David", phone: "010-0000-0004"}
-            ]
-        };
-    }
-    render(){
-        return(
-            <div>
-                <h1>Contacts</h1>
-                <ul>
-                    {this.state.contactData.map((contact, i) => {
-                        return (<ContactInfo name={contact.name}
-                                            phone={contact.phone}
-                                              key={i}/>);
-                    })}
-                </ul>
-                <ContactCreator/>
-            </div>
-        );
-    }
-}
- 
-class ContactInfo extends React.Component {
-    render() {
-        return(
-            <li>{this.props.name} {this.props.phone}</li>
-            );
     }
 }
  
